487c472b89908d612f95c1741e960863f2ff83ea
Aly Khaled <alykhaled2001@live.com>
2021-09-28 18:53:51 +0200

    Update nav bar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      sdsda
+      <header>  
+        <h1 className="logo">Collge Wiki</h1>
+        <input type="search" placeholder="Search for course" name="" id="searchInput" />
+        <nav>
+          <ul className="nav_links">
+            <li><a href="/">Courses</a></li>
+            <li><a href="/">Make a Table</a></li>
+            <li><a href="/">Guide</a></li>
+            <li><a href="/">Feedback</a></li>
+          </ul>
+        </nav>
+      </header>
+      <div className="content">
+        dasd
+      </div>
+      <footer>
+        fds
+      </footer>
     </div>
   );
 }
