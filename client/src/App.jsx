0e28a7d7cedfcd8f720fff4b4b0a0098cb629763
Aly Khaled <alykhaled2001@live.com>
2021-11-09 15:49:44 +0200

    Update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,18 @@
 import './App.scss';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { useEffect, useState } from 'react';
-import axios from 'axios';
-import CoursesPage from './components/CoursesPage/CoursesPage';
-import NavBar from './components/Navbar/NavBar';
-import Home from './components/Home/Home';
+import axios        from 'axios';
+import CoursesPage  from './components/CoursesPage/CoursesPage';
+import NavBar       from './components/Navbar/NavBar';
+import Home         from './components/Home/Home';
 import DepartmentsPage from './components/DepartmentsPage/DepartmentsPage';
-import ComingSoon from './components/ComingSoon/ComingSoon';
-import Login from './components/AdminPanel/Login/Login';
-import AdminRoute from './components/AdminPanel/AdminRoute';
-import CoursePage from './components/CoursePage/CoursePage';
-import TablePage from './components/TablePage/TablePage';
-import MyTable from './components/MyTable/MyTable';
-import UserPage from './components/UserPage/UserPage';
+import ComingSoon   from './components/ComingSoon/ComingSoon';
+import Login        from './components/AdminPanel/Login/Login';
+import AdminRoute   from './components/AdminPanel/AdminRoute';
+import CoursePage   from './components/CoursePage/CoursePage';
+import TablePage    from './components/TablePage/TablePage';
+import MyTable      from './components/MyTable/MyTable';
+import UserPage     from './components/UserPage/UserPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
 
 function App() {
@@ -61,7 +61,7 @@ function App() {
           <footer>
             Unofficial Wiki and Guide for Cairo University Faculty of Engineering<br/>
             Made by <a href="https://github.com/alykhaled">Aly Khaled</a><br/>
-            Special Thanks for Omar Yasser (Nona's Husband)
+            Special Thanks for Omar Yasser (Nona's Husband) and Ziad Mohamed (Zooz's Husband)
           </footer>
         </div>
       </Switch>
---
+++ client/src/components/UserPage/UserCourses.jsx
@@ -0,0 +1,128 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import SearchBar from '../SearchBar/SearchBar';
+import './userpage.scss';
+function UserCourses() {
+    const [completedCourses, setCompletedCourses] = useState([]);
+    const [inProgressCourses, setInProgressCourses] = useState([]);
+    const [User, setUser] = useState({});
+
+    useEffect(() => {
+        const getMe = async () => 
+        {
+            try {
+                const res = await axios.get(process.env.REACT_APP_API+"/me/",{
+                headers: {
+                    token: "Bearer " + localStorage.getItem("token"),
+                }});
+                setUser(res.data);
+                setCompletedCourses(res.data.coursesCompleted);
+                setInProgressCourses(res.data.coursesInProgress);
+                console.log(res.data);
+            } catch (error) {
+                // localStorage.removeItem("token");
+                // localStorage.removeItem("isAdmin");
+                console.log(error);
+            }
+        };
+        getMe();
+    }, [])
+    const addCourse = async (course) => 
+    {
+        await axios.put(process.env.REACT_APP_API+"/me/course/"+course._id,{},{
+            headers: {
+                token: "Bearer " + localStorage.getItem("token"),
+            }
+        });
+        completedCourses.push(course)
+        setCompletedCourses([...completedCourses]);
+    };
+
+    const addInProgressCourse = async (course) => 
+    {
+        await axios.put(process.env.REACT_APP_API+"/me/inprogress/"+course._id,{},{
+            headers: {
+                token: "Bearer " + localStorage.getItem("token"),
+            }
+        });
+        inProgressCourses.push(course)
+        setInProgressCourses([...inProgressCourses]);
+    };
+
+    const removeCourse = async (course) => 
+    {
+        await axios.put(process.env.REACT_APP_API+"/me/removecourse/"+course._id,{},{
+            headers: {
+                token: "Bearer " + localStorage.getItem("token"),
+            }
+        });
+        const index = completedCourses.indexOf(course);
+        if (index > -1) {
+            completedCourses.splice(index, 1);
+            setCompletedCourses([...completedCourses]);
+        }
+    };
+
+    const removeInProgressCourse = async (course) => 
+    {
+        await axios.put(process.env.REACT_APP_API+"/me/removeinprogress/"+course._id,{},{
+            headers: {
+                token: "Bearer " + localStorage.getItem("token"),
+            }
+        });
+        const index = inProgressCourses.indexOf(course);
+        if (index > -1) {
+            inProgressCourses.splice(index, 1);
+            setInProgressCourses([...inProgressCourses]);
+        }
+    };
+    return (
+        <div className="userpage">
+            <h1>{User.name}</h1>
+            <h1>{User.department}</h1>
+            {/* <h1>{list.description}</h1> */}
+            <div className="main">
+                <div className="coursesOption">
+                    <div className="allCourses">
+                        <h1>Add Completed courses:</h1>
+                        <SearchBar callback={addCourse} type="code"/> 
+                    </div>
+                    <div className="listCourses">
+                        <h1>List Courses:</h1>
+                        <div className="autoComplete">
+                            {completedCourses !== undefined && completedCourses.map(course => (
+                                <div>
+                                    <li>{course.code} | {course.name}</li>
+                                    <span onClick={(e) => removeCourse(course)}>
+                                        X
+                                    </span>
+                                </div>
+                            ))}
+                        </div>
+                    </div>  
+                </div>
+                <div className="coursesOption">
+                    <div className="allCourses">
+                        <h1>Add InProgress courses:</h1>
+                        <SearchBar callback={addInProgressCourse} type="code"/> 
+                    </div>
+                    <div className="listCourses">
+                        <h1>List Courses:</h1>
+                        <div className="autoComplete">
+                            {inProgressCourses !== undefined && inProgressCourses.map(course => (
+                                <div>
+                                    <li>{course.code} | {course.name}</li>
+                                    <span onClick={(e) => removeInProgressCourse(course)}>
+                                        X
+                                    </span>
+                                </div>
+                            ))}
+                        </div>
+                    </div>  
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default UserPage
