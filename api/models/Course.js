106678b720ef8ccdacc1bf20cc52361e7db59d73
Khaled Mamdouh <37771185+Khalidmamdou7@users.noreply.github.com>
2022-08-24 12:51:48 +0200

    Feat: Adding preReqHours, preReqReverse, group to Course schema to help in building course maps

--- api/models/Course.js
+++ api/models/Course.js
@@ -10,6 +10,12 @@ const CourseSchema = new mongoose.Schema({
     preReq:[{type: mongoose.Schema.Types.ObjectId , ref: "Course", required: false}],
     professor:[{type: mongoose.Schema.Types.ObjectId , ref: "Professor", required: false}],
     links:[{type: String, required: false}],
+    group:{type: String, required: false},
+
+    // Some courses have prerequisites that you must finish a number of credit hours before you can take this course.
+    preReqHours:{type: Number, required: false},
+    
+    preReqReverse:[{type: mongoose.Schema.Types.ObjectId , ref: "Course", required: false}],
 });
 
 module.exports = mongoose.model('Course',CourseSchema);
