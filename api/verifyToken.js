c4f8531a76bdcb4471c106ad2b5cdb562510d06c
Khaled Mamdouh <37771185+Khalidmamdou7@users.noreply.github.com>
2022-08-25 00:33:16 +0200

    bug: Rename the header key for the token to be authorization instead of token to follow the format of postman requests

--- api/verifyToken.js
+++ api/verifyToken.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 function verify(req,res,next){
-    const authHeader = req.headers.token;
+    const authHeader = req.headers.authorization;
     if(authHeader){
         const token = authHeader.split(' ')[1];
         const secret = process.env.SECRET_KEY;
